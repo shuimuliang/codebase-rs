8a30eb74733896b0cf440ffec72de548237a07ed
shuimuliang <shuimuliang@gmail.com>
2023-02-17 11:26:30 +0800

    chore: tokio spawn doc

--- tokio_demo/src/main.rs
+++ tokio_demo/src/main.rs
@@ -5,6 +5,10 @@ async fn foo() {
 }
 
 // https://tokio.rs/tokio/tutorial/spawning
+// Tasks in Tokio are very lightweight.
+// Under the hood, they require only a single allocation and 64 bytes of memory.
+// Applications should feel free to spawn thousands, if not millions of tasks.
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(foo());
