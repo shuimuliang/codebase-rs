7948f7b8a1233b7559f6431e1d8be933ebf7c641
shuimuliang <shuimuliang@gmail.com>
2023-03-18 21:24:16 +0800

    feat: record connection count by dashmap
    decrease count careful to avoid deadlock

--- axum_ws_ratelimit/src/main.rs
+++ axum_ws_ratelimit/src/main.rs
@@ -2,7 +2,7 @@ use {
     axum::{
         extract::{
             connect_info::ConnectInfo,
-            ws::{Message, WebSocket},
+            ws::{CloseFrame, Message, WebSocket},
             State, WebSocketUpgrade,
         },
         response::IntoResponse,
@@ -10,11 +10,7 @@ use {
     },
     axum_ws_ratelimit::state::{AppState, ShareState},
     log::debug,
-    std::{
-        env::set_var,
-        net::SocketAddr,
-        sync::{Arc, RwLock},
-    },
+    std::{borrow::Cow, env::set_var, net::SocketAddr, sync::Arc},
 };
 
 #[tokio::main]
@@ -24,14 +20,14 @@ async fn main() -> anyhow::Result<()> {
         env_logger::init();
     }
 
-    let share_state = Arc::new(RwLock::new(AppState::new()));
+    let share_state = Arc::new(AppState::new());
 
     let app = Router::new()
         .route("/", get(ws_handler))
         .with_state(share_state);
     let addr = "127.0.0.1:8080".to_owned().parse::<SocketAddr>()?;
 
-    debug!("{:?}", addr);
+    debug!("listen to: {:?}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
@@ -42,25 +38,45 @@ async fn main() -> anyhow::Result<()> {
 
 async fn ws_handler(
     ws: WebSocketUpgrade,
-    ConnectInfo(_who): ConnectInfo<SocketAddr>,
-    State(_state): State<ShareState>,
+    ConnectInfo(who): ConnectInfo<SocketAddr>,
+    State(state): State<ShareState>,
 ) -> impl IntoResponse {
-    // global check
-    ws.on_upgrade(|websocket| async move { handle_websocket(websocket).await })
+    ws.on_upgrade(move |websocket| handle_websocket(websocket, who, state))
 }
 
 // Define the function to handle WebSocket connections
-async fn handle_websocket(mut websocket: WebSocket) {
+async fn handle_websocket(mut websocket: WebSocket, who: SocketAddr, state: ShareState) {
+    // check client connection count by IpAddr
+    if state.conn_exceed_limit(&who.ip()) {
+        debug!("{} is_exceed.", who);
+        if let Err(e) = websocket
+            .send(Message::Close(Some(CloseFrame {
+                code: axum::extract::ws::close_code::NORMAL,
+                reason: Cow::from("Goodbye"),
+            })))
+            .await
+        {
+            debug!("Could not send Close due to {}, probably it is ok?", e);
+            return;
+        }
+    }
+
+    state.add_conn(who.ip());
+
     while let Some(result) = websocket.recv().await {
         match result {
             Ok(msg) => match msg {
                 Message::Text(_) => {
-                    websocket
-                        .send(Message::Text("hi".to_owned()))
-                        .await
-                        .unwrap();
+                    if let Err(e) = websocket.send(Message::Text("hi".to_owned())).await {
+                        debug!("Could not send Close due to {}, probably it is ok?", e);
+                        return;
+                    }
                 }
-                Message::Close(_c) => {}
+                Message::Close(_c) => {
+                    // decrease connection count
+                    state.remove_conn(who.ip());
+                    debug!("client close connection {}", who);
+                }
                 _ => {}
             },
             Err(e) => {
--- axum_ws_ratelimit/src/state.rs
+++ axum_ws_ratelimit/src/state.rs
@@ -1,22 +1,21 @@
-use std::{
-    collections::HashMap,
-    net::IpAddr,
-    sync::{Arc, RwLock},
+use {
+    dashmap::DashMap,
+    std::{net::IpAddr, sync::Arc},
 };
 
 const WS_CONN_LIMIT: usize = 2;
 
 #[derive(Clone, Debug)]
 pub struct AppState {
-    pub conn: HashMap<IpAddr, usize>,
+    pub conn: DashMap<IpAddr, usize>,
 }
 
-pub type ShareState = Arc<RwLock<AppState>>;
+pub type ShareState = Arc<AppState>;
 
 impl AppState {
     pub fn new() -> AppState {
         AppState {
-            conn: HashMap::new(),
+            conn: DashMap::new(),
         }
     }
     pub fn get_conn_count(&self, addr: &IpAddr) -> usize {
@@ -25,14 +24,25 @@ impl AppState {
             None => 0,
         }
     }
-    pub fn add_conn(&mut self, addr: IpAddr) {
+    pub fn add_conn(&self, addr: IpAddr) {
         *self.conn.entry(addr).or_insert(0) += 1;
     }
-    pub fn remove_conn(&mut self, addr: IpAddr) {
-        *self.conn.entry(addr).or_insert(0) -= 1;
+    pub fn remove_conn(&self, addr: IpAddr) {
+        let res = self.conn.get_mut(&addr);
+        if let Some(mut count) = res {
+            if *count > 0 {
+                *count -= 1;
+            }
+        }
+
+        self.conn.remove_if(&addr, |_, count| *count == 0);
+    }
+
+    pub fn exist_conn(&self, addr: IpAddr) -> bool {
+        self.conn.contains_key(&addr)
     }
     pub fn conn_exceed_limit(&self, addr: &IpAddr) -> bool {
-        self.get_conn_count(addr) > WS_CONN_LIMIT
+        self.get_conn_count(addr) >= WS_CONN_LIMIT
     }
 }
 
@@ -43,7 +53,7 @@ mod tests {
 
     #[test]
     fn test_state_add_conn() {
-        let mut state = AppState::new();
+        let state = AppState::new();
         let addr1 = IpAddr::from([127, 0, 0, 1]);
         // ::1
         let addr2 = IpAddr::from([0, 0, 0, 0, 0, 0, 0, 1]);
@@ -63,5 +73,38 @@ mod tests {
 
         state.add_conn(addr1);
         assert!(state.conn_exceed_limit(&addr1));
+    }
+
+    #[test]
+    fn test_state_remove_conn() {
+        let state = AppState::new();
+        let addr = IpAddr::from([127, 0, 0, 1]);
+        assert_eq!(state.exist_conn(addr), false);
+
+        // count is 1, exist
+        state.add_conn(addr);
+        assert_eq!(state.exist_conn(addr), true);
+
+        // count is 2, exist
+        state.add_conn(addr);
+        assert_eq!(state.exist_conn(addr), true);
+
+        // count decrease to 1, exist
+        state.remove_conn(addr);
+        assert_eq!(state.exist_conn(addr), true);
+
+        // count decrease to 0, not exist
+        state.remove_conn(addr);
+        assert_eq!(state.exist_conn(addr), false);
+    }
+
+    #[test]
+    fn test_state_remove_conn_non_exist() {
+        let state = AppState::new();
+        let addr = IpAddr::from([127, 0, 0, 1]);
+        assert_eq!(state.exist_conn(addr), false);
+
+        state.remove_conn(addr);
+        assert_eq!(state.exist_conn(addr), false);
     }
 }
